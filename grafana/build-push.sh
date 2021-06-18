32e4b407311ee3db3cc3c56441ca65eb495a509a
Bruno Willenborg <b.willenborg@tum.de>
2021-06-18 10:38:48 +0200

    update grafana

--- grafana/build-push.sh
+++ grafana/build-push.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-tag=${1:-7.5.8}
+tag=${1:-8.0.2}
 image="bsvr.gis.lrg.tum.de/swarmprom-grafana:$tag"
 
 echo "Image: $image"
 
 DOCKER_BUILDKIT=1 \
-docker build -t "$image" .
+docker build . \
+  -t "$image" \
+  --build-arg BASEIMAGE_TAG=$tag
 
 docker push "$image"
